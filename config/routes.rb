2eb1d335493d85369068b58e9f86cd634affc226
Eshwar <eshwar@eshwar-Inspiron-1440.(none)>
2013-12-07 13:03:31 +0530

    controller create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Gitsample::Application.routes.draw do
+  get "post/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
