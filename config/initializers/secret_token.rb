5a436059de31eebf68fde482da4fcc620bdf9f03
Eshwar <eshwar@eshwar-Inspiron-1440.(none)>
2013-12-07 12:55:21 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gitsample::Application.config.secret_key_base = '6562e54675bb196d69df99fc71db2138efd115f2743511c97c4c71bb2d44a5c02c90e4eb207b94ffc8724a0b6990643d828d226e83bb70be1b755e43e3d68b1b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gitsample::Application.config.session_store :cookie_store, key: '_gitsample_session'
